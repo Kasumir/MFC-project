1bf19733691011376d76a67e42d9cb727f3d9cfb
이지현 <wlgus7658@naver.com>
2016-12-03 18:38:49 +0900

    몬스터 충돌검사 구현. 캐릭터가 몬스터와 충돌 시 발사 불가.

--- GameObject.cpp
+++ GameObject.cpp
@@ -45,7 +45,7 @@ void GameObject::check(CList<CPoint, CPoint&>* Tile_list)
 }
 void GameObject::WD_Cehck(CList<CPoint, CPoint&>* Tile_list, CArray<CPoint,CPoint&>* Monster_point)
 {
-	for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 10; i++) {
 		CPoint wd_pos;
 		wd_pos = Water_drop[i];
 
@@ -74,17 +74,25 @@ void GameObject::WD_Cehck(CList<CPoint, CPoint&>* Tile_list, CArray<CPoint,CPoin
 		}
 
 		// 몬스터 충돌을 검사하는 코드입니다.
-		for (int i = 1; i < Monster_point->GetSize() + 1; i++) {
+		for (int p = 1; p < Monster_point->GetSize() + 1; p++) {
 			if (wd_LRstate[i] == LEFT) {
-				if ((wd_pos.x - 36 <= Monster_point->GetAt(i-1).x) && (wd_pos.x >= Monster_point->GetAt(i - 1).x) && (wd_pos.y + 24 > Monster_point->GetAt(i - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(i - 1).y))
+				if ((wd_pos.x - 36 <= Monster_point->GetAt(p-1).x) && (wd_pos.x >= Monster_point->GetAt(p - 1).x) && (wd_pos.y + 24 > Monster_point->GetAt(p - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(p - 1).y))
 				{
+					wdcount[i] = 0;
+					break;
+				}
+				else if (wdcount[i] == 16 && Monster_point->GetAt(p - 1).x > c_pos.x - 40 && Monster_point->GetAt(p - 1).x <c_pos.x + 40 && (wd_pos.y + 24 > Monster_point->GetAt(p - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(p - 1).y)) {
 					wdcount[i] = 0;
 					break;
 				}
 			}
 			else if (wd_LRstate[i] == RIGHT) {
-				if ((wd_pos.x + 36 >= Monster_point->GetAt(i - 1).x) && (wd_pos.x <= Monster_point->GetAt(i - 1).x) && (wd_pos.y + 24 > Monster_point->GetAt(i - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(i - 1).y))
+				if ((wd_pos.x + 36 >= Monster_point->GetAt(p - 1).x) && (wd_pos.x <= Monster_point->GetAt(p - 1).x) && (wd_pos.y + 24 > Monster_point->GetAt(p - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(p - 1).y))
 				{
+					wdcount[i] = 0;
+					break;
+				}
+				else if (wdcount[i] == 16 && Monster_point->GetAt(p - 1).x < c_pos.x + 40 && Monster_point->GetAt(p - 1).x >c_pos.x - 40 && (wd_pos.y + 24 > Monster_point->GetAt(p - 1).y) && (wd_pos.y - 24 < Monster_point->GetAt(p - 1).y)) {
 					wdcount[i] = 0;
 					break;
 				}
