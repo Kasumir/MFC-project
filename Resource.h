97609217b12d63c1c317cf94cd2c72a0c557bc20
이지현 <wlgus7658@naver.com>
2016-12-09 18:41:59 +0900

    Revert "시작 메뉴 추가"
    This reverts commit 4d83b8a1bbb550e40d87c78391fe1aed4e7d5198.

--- Resource.h
+++ Resource.h
@@ -24,7 +24,6 @@
 #define IDB_MONSTER1                    326
 #define IDB_WaterDrop                   327
 #define IDB_BACKGROUND1                 328
-#define IDB_StartMenu                   328
 #define IDD_SAVEDIALOG                  329
 #define IDD_LOADDIALOG                  331
 #define IDD_LOADDIALOG1                 331
